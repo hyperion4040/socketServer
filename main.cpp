3d835a29cbfc9f62472615aeffa5a5c87f0a2fc8
hyperion4040 <hyperion1987@wp.pl>
2018-11-28 20:46:08 +0100

    Added brute force version

--- main.cpp
+++ main.cpp
@@ -13,59 +13,18 @@
 //the thread function
 void *connection_handler(void *);
 
-
-int odbierzPlik(){
-    int fd =0, confd = 0,b,tot;
-    struct sockaddr_in serv_addr;
-
-    char buff[1025];
-    int num;
-
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-    printf("Socket created\n");
-
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(buff, '0', sizeof(buff));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(5000);
-
-    bind(fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    listen(fd, 10);
-
-    while(1){
-        confd = accept(fd, (struct sockaddr*)NULL, NULL);
-        if (confd==-1) {
-            perror("Accept");
-            continue;
-        }
-        FILE* fp = fopen( "provacopy.jpg", "wb");
-        tot=0;
-        if(fp != NULL){
-            while( (b = recv(confd, buff, 1024,0))> 0 ) {
-                tot+=b;
-                fwrite(buff, 1, b, fp);
-            }
-
-            printf("Received byte: %d\n",tot);
-            if (b<0)
-                perror("Receiving");
-
-            fclose(fp);
-        } else {
-            perror("File");
-        }
-        close(confd);
-    }
-
-    return 0;
-}
-
-
-
+int sendText();
+int sendFile();
 int main(int argc , char *argv[])
 {
+
+
+//    return sendText();
+    return sendFile();
+
+}
+
+int sendText() {
     int socket_desc , client_sock , c , *new_sock;
     struct sockaddr_in server , client;
 
@@ -170,6 +129,54 @@ void *connection_handler(void *socket_desc)
 
     //Free the socket pointer
     free(socket_desc);
+
+    return 0;
+}
+
+int sendFile(){
+    int fd =0, confd = 0,b,tot;
+    struct sockaddr_in serv_addr;
+
+    char buff[1025];
+    int num;
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    printf("Socket created\n");
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(buff, '0', sizeof(buff));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5000);
+
+    bind(fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    listen(fd, 10);
+
+    while(1){
+        confd = accept(fd, (struct sockaddr*)NULL, NULL);
+        if (confd==-1) {
+            perror("Accept");
+            continue;
+        }
+        FILE* fp = fopen( "provacopy.txt", "wb");
+        tot=0;
+        if(fp != NULL){
+            while( (b = recv(confd, buff, 1024,0))> 0 ) {
+                tot+=b;
+                fwrite(buff, 1, b, fp);
+            }
+
+            printf("Received byte: %d\n",tot);
+            if (b<0)
+                perror("Receiving");
+
+            fclose(fp);
+        } else {
+            perror("File");
+        }
+        close(confd);
+    }
 
     return 0;
 }
